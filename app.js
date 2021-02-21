2f3677e75a11e12d8b2b479d945b189c6f151ad6
Oliver N <72499511+NemesOliver@users.noreply.github.com>
2021-02-21 16:39:54 +0000

    Export teppan station

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 //import teppanSection
-import teppan from "./data/teppanStation.js";
+import teppanStation from "./data/teppanStation.js";
 
-const printData = () => {
-  console.log(teppan);
-};
-printData();
+// select individual dishes
+// const yakiSoba = teppanStation[0].yakiSoba;
+// const yasaiYakiSoba = teppanStation[1].yasaiYakiSoba;
+// const padThai = teppanStation[2].padThai;
+// const teriyaki = teppanStation[3].teriyaki;
+// const yakiUdon = teppanStation[4].yakiUdon;
+// const gingerChicken = teppanStation[5].gingerChicken;
+console.log(teppanStation);
--- data/teppanStation.js
+++ data/teppanStation.js
@@ -113,11 +113,4 @@ const teppanStation = [
   },
 ];
 
-// const yakiSoba = teppanStation[0].yakiSoba;
-// const yasaiYakiSoba = teppanStation[1].yasaiYakiSoba;
-// const padThai = teppanStation[2].padThai;
-// const teriyaki = teppanStation[3].teriyaki;
-// const yakiUdon = teppanStation[4].yakiUdon;
-// const gingerChicken = teppanStation[5].gingerChicken;
-
 export default teppanStation;
