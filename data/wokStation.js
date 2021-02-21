6d8b48e5bdd532e03c02418d65683491fda07ba9
Oliver N <72499511+NemesOliver@users.noreply.github.com>
2021-02-21 17:08:38 +0000

    wok station boilerplate

---
+++ data/wokStation.js
@@ -0,0 +1,107 @@
+// par levels per sales
+const wokStation = [
+  {
+    yakiSoba: {
+      parLevels: {
+        twoThousand: 15,
+        threeThousand: 20,
+        fourThousand: 25,
+        fiveThousand: 30,
+        sixThousand: 30,
+        sevenThousand: 35,
+        eightThousand: 45,
+        nineThousand: 60,
+        tenThousand: 65,
+        elevenThousand: 70,
+        twelveThousand: 80,
+      },
+    },
+  },
+  {
+    yasaiYakiSoba: {
+      parLevels: {
+        twoThousand: 5,
+        threeThousand: 7,
+        fourThousand: 10,
+        fiveThousand: 15,
+        sixThousand: 15,
+        sevenThousand: 20,
+        eightThousand: 20,
+        nineThousand: 20,
+        tenThousand: 25,
+        elevenThousand: 30,
+        twelveThousand: 30,
+      },
+    },
+  },
+  {
+    padThai: {
+      parLevels: {
+        twoThousand: 20,
+        threeThousand: 25,
+        fourThousand: 40,
+        fiveThousand: 40,
+        sixThousand: 50,
+        sevenThousand: 55,
+        eightThousand: 65,
+        nineThousand: 70,
+        tenThousand: 80,
+        elevenThousand: 90,
+        twelveThousand: 95,
+      },
+    },
+  },
+  {
+    teriyaki: {
+      parLevels: {
+        twoThousand: 10,
+        threeThousand: 15,
+        fourThousand: 20,
+        fiveThousand: 20,
+        sixThousand: 25,
+        sevenThousand: 25,
+        eightThousand: 30,
+        nineThousand: 35,
+        tenThousand: 40,
+        elevenThousand: 40,
+        twelveThousand: 45,
+      },
+    },
+  },
+  {
+    yakiUdon: {
+      parLevels: {
+        twoThousand: 8,
+        threeThousand: 10,
+        fourThousand: 15,
+        fiveThousand: 15,
+        sixThousand: 20,
+        sevenThousand: 20,
+        eightThousand: 25,
+        nineThousand: 25,
+        tenThousand: 30,
+        elevenThousand: 35,
+        twelveThousand: 35,
+      },
+    },
+  },
+  {
+    gingerChicken: {
+      parLevels: {
+        twoThousand: 6,
+        threeThousand: 8,
+        fourThousand: 10,
+        fiveThousand: 12,
+        sixThousand: 15,
+        sevenThousand: 20,
+        eightThousand: 25,
+        nineThousand: 25,
+        tenThousand: 30,
+        elevenThousand: 35,
+        twelveThousand: 35,
+      },
+    },
+  },
+];
+
+export default wokStation;
