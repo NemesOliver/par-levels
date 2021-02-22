3a9a8ad4a724c1ed71d6444d5c1b94840b7744e5
Oliver N <72499511+NemesOliver@users.noreply.github.com>
2021-02-22 19:20:46 +0000

    comment updates

--- app.js
+++ app.js
@@ -3,26 +3,7 @@ import teppanStation from "./data/teppanStation.js";
 import wokStation from "./data/wokStation.js";
 import fryStation from "./data/fryStation.js";
 
-// select individual dishes - teppan
-
-// const yakiSoba = teppanStation[0].yakiSoba;
-// const yasaiYakiSoba = teppanStation[1].yasaiYakiSoba;
-// const padThai = teppanStation[2].padThai;
-// const teriyaki = teppanStation[3].teriyaki;
-// const yakiUdon = teppanStation[4].yakiUdon;
-// const gingerChicken = teppanStation[5].gingerChicken;
-
-// select individual dishes - fry
-
-// const chickenKatsu = fryStation[0].chickenKatsu;
-// const yasaiKatsu = fryStation[1].yasaiKatsu;
-// const pankoSeitan = fryStation[2].pankoSeitan;
-// const chilliSquid = fryStation[3].chilliSquid;
-// const ebiKatsu = fryStation[4].ebiKatsu;
-// const duckGyoza = fryStation[5].duckGyoza;
-// const ebiGyozaPieces = fryStation[5].ebiGyozaPieces;
-
-// add prep levels to table
+// add prep levels to table with switch statement
 const printPrepLevels = () => {
   let wokCounter = 1;
   let teppanCounter = 1;
