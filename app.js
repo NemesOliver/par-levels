d400914cba4fcf1ff3abbdf11e9cd4a490d29721
Oliver N <72499511+NemesOliver@users.noreply.github.com>
2021-03-04 14:50:22 +0000

    get array of parLevels

--- app.js
+++ app.js
@@ -3,25 +3,40 @@ import teppanStation from "./data/teppanStation.js";
 import wokStation from "./data/wokStation.js";
 import fryStation from "./data/fryStation.js";
 
+// Get names
 const getDishesNames = (dishes) => {
   let names = [];
   dishes.forEach((dish) => {
-    names.push(dish.dish.name);
+    let name = dish.dish.name;
+    names.push(name);
   });
   return names;
 };
 
-// returns an array
+// returns an array with names
 const teppanDishesNames = getDishesNames(teppanStation);
 const wokDishesNames = getDishesNames(wokStation);
 const fryDishesNames = getDishesNames(fryStation);
 
+// Get par levels
 const getDishesParLevels = (dishes) => {
+  let parLevelsAmounts = [];
+  let parLevels = null;
   dishes.forEach((dish) => {
-    console.log(dish.dish.parLevels["twoThousand"]);
+    parLevels = dish.dish.parLevels;
+    parLevelsAmounts.push(parLevels);
   });
+  return parLevelsAmounts;
 };
-getDishesParLevels(teppanStation);
+
+// returns an array with par levels
+const teppanParLevels = getDishesParLevels(teppanStation);
+const wokParLevels = getDishesParLevels(wokStation);
+const fryParLevels = getDishesParLevels(fryStation);
+
+console.log(teppanParLevels);
+
+
 
 // document.querySelector("#wokTable").innerHTML = "";
 //           wokStation.forEach((object) => {
