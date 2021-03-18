b95517f8f59839c2f003a00512e20f5e31fd5cc6
Oliver N <72499511+NemesOliver@users.noreply.github.com>
2021-03-18 14:07:10 +0000

    display data in a table

---
+++ data/wok.js
@@ -0,0 +1,68 @@
+//array of wok dishes
+const wok = [
+  {
+    name: "Raisukaree",
+    parLevels: {
+      twoThousand: 10,
+      threeThousand: 15,
+      fourThousand: 20,
+      fiveThousand: 20,
+      sixThousand: 25,
+      sevenThousand: 30,
+      eightThousand: 35,
+      nineThousand: 40,
+      tenThousand: 45,
+      elevenThousand: 50,
+      twelveThousand: 55,
+    },
+  },
+  {
+    name: "Firecracker",
+    parLevels: {
+      twoThousand: 10,
+      threeThousand: 15,
+      fourThousand: 20,
+      fiveThousand: 25,
+      sixThousand: 30,
+      sevenThousand: 35,
+      eightThousand: 40,
+      nineThousand: 45,
+      tenThousand: 50,
+      elevenThousand: 55,
+      twelveThousand: 60,
+    },
+  },
+  {
+    name: "Mixed mushrooms",
+    parLevels: {
+      twoThousand: 10,
+      threeThousand: 12,
+      fourThousand: 15,
+      fiveThousand: 15,
+      sixThousand: 15,
+      sevenThousand: 20,
+      eightThousand: 25,
+      nineThousand: 25,
+      tenThousand: 30,
+      elevenThousand: 35,
+      twelveThousand: 35,
+    },
+  },
+  {
+    name: "Wok greens",
+    parLevels: {
+      twoThousand: 7,
+      threeThousand: 10,
+      fourThousand: 12,
+      fiveThousand: 15,
+      sixThousand: 15,
+      sevenThousand: 20,
+      eightThousand: 20,
+      nineThousand: 25,
+      tenThousand: 25,
+      elevenThousand: 30,
+      twelveThousand: 35,
+    },
+  },
+];
+export default wok;
--- scripts/parLevels.js
+++ scripts/parLevels.js
@@ -1,2 +1,32 @@
 //import Stations data
-// import teppanStation from "../data/teppanStation.js";
+import teppan from "../data/teppan.js";
+import wok from "../data/wok.js";
+import fry from "../data/fry.js";
+
+// select dropdown menu
+const dropDown = document.querySelector("#drop-down");
+
+const teppanTableId = document.querySelector("#teppanTable");
+const wokTableId = document.querySelector("#wokTable");
+const fryTableId = document.querySelector("#fryTable");
+
+const createTable = (section, sectionTable) => {
+  let option = dropDown.value;
+  sectionTable.innerHTML = "";
+  section.forEach((dish) => {
+    const table = document.createElement("tr");
+    table.innerHTML = `
+    <th scope="row">1</th>
+    <td>${dish.name}</td>
+    <td>${dish.parLevels[option]}</td>
+    `;
+    sectionTable.appendChild(table);
+  });
+};
+
+const displayTable = () => {
+  createTable(wok, wokTableId);
+  createTable(teppan, teppanTableId);
+  createTable(fry, fryTableId);
+};
+dropDown.addEventListener("change", displayTable);
