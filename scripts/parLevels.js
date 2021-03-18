0e06e9e435f1a556a91ac7872ce7620e76fdf836
Oliver N <72499511+NemesOliver@users.noreply.github.com>
2021-03-18 14:09:20 +0000

    add counter

--- scripts/parLevels.js
+++ scripts/parLevels.js
@@ -6,21 +6,24 @@ import fry from "../data/fry.js";
 // select dropdown menu
 const dropDown = document.querySelector("#drop-down");
 
+// select individual section html table element
 const teppanTableId = document.querySelector("#teppanTable");
 const wokTableId = document.querySelector("#wokTable");
 const fryTableId = document.querySelector("#fryTable");
 
 const createTable = (section, sectionTable) => {
+  let counter = 1;
   let option = dropDown.value;
   sectionTable.innerHTML = "";
   section.forEach((dish) => {
     const table = document.createElement("tr");
     table.innerHTML = `
-    <th scope="row">1</th>
+    <th scope="row">${counter}</th>
     <td>${dish.name}</td>
     <td>${dish.parLevels[option]}</td>
     `;
     sectionTable.appendChild(table);
+    counter += 1;
   });
 };
 
